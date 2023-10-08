6cdeeed5907fef4c398d3c0c003b6cfd2b54fdff
Никита Глухих <gluhih20003@mail.ru>
2023-10-08 22:00:26 +0300

    initial commit

---
+++ backend/momsproject/UserManagementService/src/main/java/UserManagementService.java
@@ -0,0 +1,7 @@
+import org.springframework.boot.SpringApplication;
+
+public class UserManagementService {
+    public static void main(String[] args) {
+        SpringApplication.run(UserManagementService.class);
+    }
+}
