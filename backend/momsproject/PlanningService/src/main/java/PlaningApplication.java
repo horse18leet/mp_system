26a9cbee2749bf9f17368ed9a301736c87f6face
Никита Глухих <gluhih20003@mail.ru>
2023-10-10 16:59:24 +0300

    initial commit

---
+++ backend/momsproject/PlanningService/src/main/java/PlaningApplication.java
@@ -0,0 +1,7 @@
+import org.springframework.boot.SpringApplication;
+
+public class PlaningApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PlaningApplication.class);
+    }
+}
