629159a8d40768e426bf4b7b708b38c5e57faf4f
Vadim Elsukov <elsukov.vadim@gmail.com>
2023-10-23 22:19:41 +0300

    fix: productSchema

--- frontend/mp_front/schemes/createProductSchema.ts
+++ frontend/mp_front/schemes/createProductSchema.ts
@@ -7,12 +7,12 @@ const schema = Joi.object({
         "string.max": "Максимальная длина 50 символов",
         "any.required": "Поле обязательно к заполнению",
     }),
-    productCategory: Joi.string().required().min(2).max(50).messages({
-        "string.empty": "Поле обязательно к заполнению",
-        "string.min": "Минимальная длина 2 символа",
-        "string.max": "Максимальная длина 50 символов",
-        "any.required": "Поле обязательно к заполнению",
-    }),
+    // productCategory: Joi.string().required().min(2).max(50).messages({
+    //     "string.empty": "Поле обязательно к заполнению",
+    //     "string.min": "Минимальная длина 2 символа",
+    //     "string.max": "Максимальная длина 50 символов",
+    //     "any.required": "Поле обязательно к заполнению",
+    // }),
     productPrice: Joi.string().required().pattern(/^\d+$/).min(1).max(12).messages({
         "string.pattern.base": "Стоимость должна содержать только числа",
         "string.empty": "Поле обязательно к заполнению",
@@ -20,12 +20,11 @@ const schema = Joi.object({
         "string.max": `Максимальная длина 30 символов`,
         "any.required": "Поле обязательно к заполнению",
     }),
-    productLink: Joi.string().required().pattern(/^https?:\/\/(www\.)?[a-zA-Z\d]+\.[\w\-._~:/?#[\]@!$&'()*+,;=]{2,}#?$/).min(10).max(100).messages({
+    productLink: Joi.string().optional().empty('').default(null).pattern(/^https?:\/\/(www\.)?[a-zA-Z\d]+\.[\w\-._~:/?#[\]@!$&'()*+,;=]{2,}#?$/).min(10).max(100).messages({
         "string.pattern.base": "Ссылка должна соответствовать формату: https://example...",
-        "string.empty": "Поле обязательно к заполнению",
         "string.min": `Минимальная длина 9 символ`,
         "string.max": `Максимальная длина 100 символов`,
-        "any.required": "Поле обязательно к заполнению",
+        "any.optional": "Поле необязательно к заполнению"
     }),
 });
 // .pattern(/^https?:\/\/(www\.)?[a-zA-Z\d]+\.[\w\-._~:/?#[\]@!$&'()*+,;=]{2,}#?$/).min(10).max(100)
